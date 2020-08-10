a824b514858ce3a5bb11eed6a9ce1515531a3ca2
Barbara Gilson <bgilson@sis125.berkeley.edu>
2020-08-10 14:59:48 -0700

    Tier 1

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,22 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+		long myNum = num;
+		int length = String.valueOf(num).length();
+		long sum = 0;
+		long digit = 0;
+		
+		for(int i=1; i<=length; i++) {
+			digit = (myNum % (long) Math.pow(10, i)) / (long) Math.pow(10, i-1);
+			sum += Math.pow(digit,length);
+			myNum -= digit*Math.pow(10, i-1);
+		}
+		
+		if (sum == num) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 }
